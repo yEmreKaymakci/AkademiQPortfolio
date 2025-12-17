f060e9fa1aaeba66d7f68a639887ecc370236567
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-12-17 22:15:11 +0300

    Adding admin Page

---
+++ AkademiQPortfolio/Controllers/SkillController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult SkillList()
+        {
+            return View();
+        }
+    }
+}

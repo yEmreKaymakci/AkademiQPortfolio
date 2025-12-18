4ec4a01cd3d372f1a8d0c3d3dd35bb681fa3ac7f
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-12-18 21:29:04 +0300

    Adding Skill Pages

--- AkademiQPortfolio/Controllers/SkillController.cs
+++ AkademiQPortfolio/Controllers/SkillController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AkademiQPortfolio.Data;
+using AkademiQPortfolio.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AkademiQPortfolio.Controllers
 {
     public class SkillController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public SkillController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult SkillList()
         {
+            var values = _context.Skills.ToList();  
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateSkill()
+        {
             return View();
+        }
+        [HttpPost]
+        public IActionResult CreateSkill(Skill skill)
+        {
+            _context.Skills.Add(skill);
+            _context.SaveChanges();
+            return RedirectToAction("SkillList");
         }
     }
 }

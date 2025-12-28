a0aab67e6434163346013b32a6cc542dbe310c44
yEmreKaymakci <yunusemrekymkci@gmail.com>
2025-12-28 16:32:39 +0300

    feat(admin): add education and experience management with CRUD operations
    - Added Educations and Experiences controllers
    - Implemented create, update and list views for education records
    - Implemented create, update and list views for experience records

---
+++ AkademiQPortfolio/Controllers/EducationsController.cs
@@ -0,0 +1,55 @@
+﻿using AkademiQPortfolio.Data;
+using AkademiQPortfolio.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class EducationsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EducationsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.Educations.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateEducation(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateEducation(Education education)
+        {
+            _context.Educations.Add(education);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateEducation(int id)
+        {
+            var value = _context.Educations.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateEducation(Education education)
+        {
+            _context.Educations.Update(education);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteEducation(int id)
+        {
+            var value = _context.Educations.Find(id);
+            _context.Educations.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AkademiQPortfolio/Controllers/ExperiencesController.cs
@@ -0,0 +1,54 @@
+﻿using AkademiQPortfolio.Data;
+using AkademiQPortfolio.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkademiQPortfolio.Controllers
+{
+    public class ExperiencesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ExperiencesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            _context.Experiences.Update(experience);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = _context.Experiences.Find(id);
+            _context.Experiences.Remove(value);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
